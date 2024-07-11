bd66881c66f3fc87fb6e83caf6f00eea4c2c82df
Omar Yahya <omaryahyasaeed@gamil.com>
2024-07-11 11:03:16 +0300

    I did the first step in the program. I defined the program functions: - add_task - mark_task_complete - view_tasks I prepared the display message for the user and made the loop condition on the message

--- Tasks Manager.py
+++ Tasks Manager.py
@@ -1,1 +1,34 @@
-print("")
+import os
+os.system('cls') #مسح المسارات التي تظهر في الكونسول
+
+def add_task(item): #
+    pass
+def mark_task_complete():
+    pass
+def view_tasks():
+    ...
+
+message = """1- add tasks to a list
+2- mark task as complete
+3- view tasks
+4- Quit"""
+# 
+tasks = []
+
+while(True):
+    print(message)
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        # add task to tasks list
+        add_task()
+    elif choice == "2":
+        # علم على المهمة ان المهمة تم اكمالها
+        mark_task_complete()
+    elif choice == "3":
+        # عرض جميع المهام الموجودة
+        view_tasks()
+    elif choice == "4":
+        # الخروج من البرنامج
+        break
+    else:
+        print("Invalid choice, please enter a number between 1 and 4")
