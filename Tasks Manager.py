02c48ec0ed1d65289334e4294d2c6d74e56ff9a0
Omar Yahya <omaryahyasaeed@gamil.com>
2024-07-15 22:39:41 +0300

    Add the code of mark_task_complete and view_tasks methods and end the program

--- Tasks Manager.py
+++ Tasks Manager.py
@@ -9,11 +9,60 @@ def add_task(): # add task to the list
     # add task to the list of tasks
     tasks.append(task_info)
     print("Task added to the lsit successfuly")
-    
+
+  
 def mark_task_complete():
-    pass
+    # get list of incomplete tasks
+    incomplete_tasks = [task for task in tasks if task[ "completed"] == False ]
+    # print(incomplete_tasks)
+
+    # للخروج من الدالة اذا لم يوجد مهام للتعليم عليها انها مكتملة
+    if len(incomplete_tasks) == 0: #another way => if not incomplete_tasks:
+        print("No tasks to mark as complete")
+        print("-"*25)
+        return
+    
+    #show incomplete tasks to the user
+    for i,task in enumerate(incomplete_tasks):
+        print(f"{i+1}- {task['task']}")
+    print("-"*25)
+    
+    # get the task number from the user
+    task_number = int(input("Choose the task to complete"))
+
+    # mark the task as completed
+    incomplete_tasks [task_number - 1]["completed"] = True
+    # aliasing
+
+    # print a message to the user
+    print("Task marked successfuly")
+
+    #  pass
+
+###--- Function to view the tasks ---###
 def view_tasks():
-    ...
+    # if there are no tasks, print a message and write return
+    if not tasks:
+        print("No tasks to view")
+        print("-"*25)
+        return
+    
+    for i,task in enumerate(tasks):
+
+        # if task["completed"]:
+        #     status = "✔️"
+        # else:
+        #     status = "❌"
+        status = "✔️" if task["completed"] else "❌"  # same work as above
+
+        print(f"{i+1}. {task['task']} {status}")
+    
+    print("-"*25)
+    # ❌
+    # ✔️  #         شعار الويندوز   +   .
+
+    #...  # ... هي نفسها  pass
+
 
 message = """1- add tasks to a list
 2- mark task as complete
@@ -25,6 +74,7 @@ tasks = []
 while(True):
     print(message)
     choice = input("Enter your choice: ")
+    print("-"*25)
     if choice == "1":
         # add task to tasks list
         add_task()
